11bb8453dabc0ad1f5d584919af84682c3305bee
Xochitl <xocaid@gmail.com>
2024-06-01 13:21:19 -0700

    basic stage of db

--- src/pages/movieCard.tsx
+++ src/pages/movieCard.tsx
@@ -1,37 +1,33 @@
 import React from "react";
+import styles from '../styles/movieCard.module.css';
+
+export interface MovieInfo {
+    Poster: string,
+    Title: string,
+    Type: string,
+    Year: string,
+    Plot: string
+}
 
 interface Props {
-    movieInfo: {
-        Poster: string,
-        Title: string,
-        imdbRating: string,
-        Year: string,
-        Plot: string
-    }
+    movieInfo: MovieInfo;
 }
 
 const MovieCard = ({ movieInfo }: Props) => {
     return (
-        <div>
-            One Div Box for One Entry
-            <div>
-                Poster
-                <img src={movieInfo.Poster} alt={movieInfo.Title} className="movie_poster"></img>
-                <div>
-                    Title
+        <div className={styles.movieCardDiv}>
+            <div className={styles.movieCardPoster}>
+                <img src={movieInfo.Poster} alt={movieInfo.Title} />
+            </div>
+            <div className={styles.movieCardSumm}>
+                <div className={styles.movieCardTitle}>
                     <h1>{movieInfo.Title}</h1>
-                    <div>
-                        IMDB Rating - place next to year
-                        <h3>{movieInfo.imdbRating}</h3>
-                        <div>
-                            Year - place next to imdb rating
-                            {movieInfo.Year}
-                            <div>
-                                Plot - brief plot & add a see more... to see the entire plot
-                                <p>{movieInfo.Plot}</p>
-                            </div>
-                        </div>
-                    </div>
+                </div>
+                <div>
+                    <h4>{movieInfo.Type[0].toUpperCase() + movieInfo.Type.slice(1)}</h4>
+                </div>
+                <div>
+                    <h4>{movieInfo.Year}</h4>
                 </div>
             </div>
         </div>
